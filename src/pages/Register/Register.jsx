ffc2b514071f4452bd1d07681d32584dee739811
ArfanDwiSukmajaya <jayaarfan92@gmail.com>
2023-11-12 20:57:15 +0700

    Update field register

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,13 +1,53 @@
-import { Button, Select, Flex, Text, FormControl, FormLabel, Heading, InputGroup, Input, Stack, Icon, Image, InputRightElement } from "@chakra-ui/react";
+import { Button, Flex, Text, FormControl, InputGroup, Heading, Input, Stack, Image, InputRightElement, Icon, Select } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { HiEye, HiEyeOff } from "react-icons/hi";
 import SmallWithNavigation from "../../components/Footer";
 import { useState } from "react";
-import SignUpField from "../../components/Input/Input";
 
-export default function Register() {
+export default function Login() {
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+
+    if (name === "email") {
+      const emailRegex = /^\S+@\S+\.\S+$/;
+      setError((prevError) => ({
+        ...prevError,
+        email: emailRegex.test(value) ? "" : "Please enter a valid email address",
+      }));
+    } else if (name === "password") {
+      setError((prevError) => ({
+        ...prevError,
+        password: value.length >= 6 ? "" : "Password must be at least 6 characters",
+      }));
+    } else {
+      setError((prevError) => ({ ...prevError, [name]: "" }));
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!formData.password || formData.password.length < 6) {
+      setError((prevError) => ({ ...prevError, password: "Password must be at least 6 characters" }));
+      return;
+    }
+
+    console.log("Form data submitted:", formData.email, formData.password);
+  };
 
   return (
     <>
@@ -22,59 +62,41 @@ export default function Register() {
                 </Text>
               </Link>
             </Heading>
-            <Heading fontSize={"2xl"}>Sign Up to your account</Heading>
+            <Heading fontSize={"2xl"}>Sign in to your account</Heading>
+            <form onSubmit={handleSubmit}>
+              <FormControl id="email">
+                <Input type="email" name="email" placeholder="Email" onChange={handleInputChange} />
+                {error.email && <Text color="red.500">{error.email}</Text>}
+              </FormControl>
 
-            <SignUpField type="text" placeholder="Full Name" />
-            <SignUpField type="email" placeholder="Email" />
-            <SignUpField type="text" placeholder="No Hp" />
+              <FormControl id="password" py={5}>
+                <InputGroup size="md">
+                  <Input type={show ? "text" : "password"} name="password" placeholder="Password" onChange={handleInputChange} />
+                  <InputRightElement width="4.5rem">
+                    <Button size="lg" onClick={handleClick} variant="">
+                      <Icon ml={7} as={show ? HiEyeOff : HiEye} />
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                {error.password && <Text color="red.500">{error.password}</Text>}
+              </FormControl>
 
-            <Flex>
-              <InputGroup size="md">
-                <Input type={show ? "text" : "password"} name="password" placeholder="Password" />
-                <InputRightElement width="4.5rem">
-                  <Button size="lg" onClick={handleClick} variant="">
-                    <Icon ml={7} as={show ? HiEyeOff : HiEye} />
-                  </Button>
-                </InputRightElement>
-              </InputGroup>
-              <Text as="span" color="red" ml={1}>
-                *
-              </Text>
-            </Flex>
-
-            <SignUpField type="number" placeholder="Kode Pos" />
-
-            <Flex>
-              <Select placeholder="Kecamatan">
-                <option>United Arab Emirates</option>
-                <option>Nigeria</option>
+              <Select placeholder="Role">
+                <option>User</option>
+                <option>Foudation</option>
               </Select>
-              <Text as="span" color="red" ml={1}>
-                *
-              </Text>
-            </Flex>
 
-            <Flex>
-              <Select placeholder="Kelurahan">
-                <option>United Arab Emirates</option>
-                <option>Nigeria</option>
-              </Select>
-              <Text as="span" color="red" ml={1}>
-                *
-              </Text>
-            </Flex>
-
-            <Stack spacing={6}>
-              <Button colorScheme={"orange"} variant={"solid"}>
-                Sign Up
-              </Button>
-            </Stack>
-
+              <Stack spacing={6} mt={5}>
+                <Button type="submit" colorScheme="orange" variant="solid">
+                  Sign Up
+                </Button>
+              </Stack>
+            </form>
             <Stack pt={6}>
-              <Text align={"center"}>
-                Already a user?{" "}
+              <Text align="center">
+                New to our service?{" "}
                 <Link to="/login">
-                  <Text as={"span"} color={"orange.400"}>
+                  <Text as="span" color="orange.400">
                     Sign In
                   </Text>
                 </Link>
@@ -82,12 +104,14 @@ export default function Register() {
             </Stack>
           </Stack>
         </Flex>
-
         <Flex flex={1}>
-          <Image alt={"Login Image"} objectFit={"cover"} src={"https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"} />
+          <Image
+            alt="Login Image"
+            objectFit="cover"
+            src="https://images.unsplash.com/photo-1509822929063-6b6cfc9b42f2?auto=format&fit=crop&q=80&w=3270&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1352&q=80"
+          />
         </Flex>
       </Stack>
-
       <SmallWithNavigation />
     </>
   );
