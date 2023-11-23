49caebca2a4b4e819655d53d5cb3f09c08de0df6
ArfanDwiSukmajaya <jayaarfan92@gmail.com>
2023-11-23 11:34:06 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import useTokenStore from "./config/store";
 import CompleteUserData from "./pages/CompleteUserData/CompleteUserData";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Login from "./pages/Login/Login";
-import NotFound from "./pages/NotFound/notFound";
+import NotFound from "./pages/NotFound/NotFound";
 import Register from "./pages/Register/Register";
 import HomePage from "./pages/homepage/HomePage";
 
